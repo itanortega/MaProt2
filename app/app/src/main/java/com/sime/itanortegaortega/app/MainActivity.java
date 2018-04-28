a7a4dc058418cefea60fd0c84f887425e97aea52
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-04-28 12:09:33 -0500

    Cambio de actividad

--- app/app/src/main/java/com/sime/itanortegaortega/app/MainActivity.java
+++ app/app/src/main/java/com/sime/itanortegaortega/app/MainActivity.java
@@ -25,172 +25,76 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private final static String DOMAIN = "http://181.62.161.249:41062/www/api/";
     private static String LOCAL = "";
-
     private CategoriasAdapter adapter;
     private GridView Gv_Categorias;
+    private ExecutorService queue = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("debugapp", "actividad main");
-        /*LOCAL = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
-
         Gv_Categorias =  (GridView)findViewById(R.id.Gv_Categorias);
         Gv_Categorias.setOnItemClickListener(this);
 
-        if(existeArchivoVersion()){
-            Log.d("debugapp", "existe el archivo");
-            if(versionesDiferentes()){
-                Log.d("debugapp", "versiones diferentes del archivo");
-                //descargarTodo();
-            }
-        }else{
-            Log.d("debugapp", "se debe descargar todo");
-            descargarTodo();
-        }
-        
-        cargarCategorias();*/
+        LOCAL = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
+
+        cargarListaCategorias();
     }
 
-    private void cargarCategorias() {
-    }
+    private void cargarListaCategorias() {
+        final String strUrl_Local = LOCAL + "words.json";
 
-    private void descargarTodo() {
-        ExecutorService queue = Executors.newSingleThreadExecutor();
-
-        Runnable thread = new Runnable() {
-            URL urlV = null;
-            URL urlP = null;
-            boolean iguales = true;
-
+        final Runnable thread = new Runnable() {
             @Override
             public void run() {
+                CAFData data = null;
+                data = CAFData.dataWithContentsOfFile(strUrl_Local);
                 try {
-                    urlV = new URL(DOMAIN + "version.json");
-                    urlP = new URL(DOMAIN + "words.json");
+                    JSONObject root = new JSONObject(data.toText());
+                    JSONArray catJson = root.getJSONArray("categorias");
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
+                    final JSONArray categoriasJson = catJson;
 
-                CAFData versionData;
-                CAFData wordsData;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<Categoria> categorias = new ArrayList<>();
+                            Categoria c;
+                            JSONObject categoriaJson;
 
-                if(urlV != null && urlP !=null){
-                    versionData = CAFData.dataWithContentsOfURL(urlV);
-                    wordsData = CAFData.dataWithContentsOfURL(urlP);
-
-                    try {
-                        JSONObject jsonVersion = new JSONObject(versionData.toText());
-                        JSONObject jsonWordsData = new JSONObject(wordsData.toText());
-
-                        versionData.writeToFile(LOCAL + "version.json", true);
-                        wordsData.writeToFile(LOCAL + "words.json", true);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-
-        queue.execute(thread);
-        Log.d("debugapp", "descargar todo");
-    }
-
-    private boolean versionesDiferentes() {
-        final String urlWeb = DOMAIN + "version.json";
-        final String urlLocal = LOCAL + "version.json";
-        final boolean[] res = {true};
-        ExecutorService queue = Executors.newSingleThreadExecutor();
-
-        Runnable thread = new Runnable() {
-            URL urlW = null;
-            URL urlL = null;
-            boolean iguales = true;
-
-            @Override
-            public void run() {
-                try {
-                    urlW = new URL(urlWeb);
-                    urlL = new URL(urlLocal);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
-                Log.d("debugapp", String.valueOf(urlW));
-                Log.d("debugapp", String.valueOf(urlL));
-                CAFData web;
-                CAFData local;
-
-                if(urlW != null && urlL !=null){
-                    web = CAFData.dataWithContentsOfURL(urlW);
-                    local = CAFData.dataWithContentsOfURL(urlL);
-
-                    try {
-                        JSONObject webJson = new JSONObject(web.toText());
-                        JSONObject localJson = new JSONObject(local.toText());
-                        Log.d("debugapp", webJson.toString());
-                        Log.d("debugapp", localJson.toString());
-                        if(webJson.toString().equals(localJson.toString())){
-                            iguales = false;
-                            Log.d("debugapp", String.valueOf(iguales));
-                        }
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                res[0] = iguales;
+                            for (int i = 0; i < categoriasJson.length(); i++) {
+                                try {
+                                    categoriaJson = categoriasJson.getJSONObject(i);
+                                    c = new Categoria(i, categoriaJson.getString("nombre").toString(), "", "");
+                                    categorias.add(c);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        });
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
 
-        queue.execute(thread);
-        return res[0];
-    }
-
-    private boolean existeArchivoVersion() {
-        final String urlLocal = LOCAL + "version.json";
-        final boolean[] res = {false};
-        ExecutorService queue = Executors.newSingleThreadExecutor();
-
-        Runnable thread = new Runnable() {
-            URL urlW = null;
-            URL urlL = null;
-            boolean existe = false;
-
-            @Override
-            public void run() {
-                try {
-                    urlL = new URL(urlLocal);
-                } catch (MalformedURLException e) {
+                            final ArrayList<Categoria> categoriasV = categorias;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter = new CategoriasAdapter(MainActivity.this, categoriasV);
+                                    Gv_Categorias.setAdapter(adapter);
+                                }
+                            });
+                        }
+                    });
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                if (urlL != null) {
-                    existe = true;
-                }
-
-                res[0] = existe;
             }
         };
 
         queue.execute(thread);
-        return res[0];
     }
-
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) { Log.d("debugapp", "clic");
         Categoria item = (Categoria) parent.getItemAtPosition(position);
 
         Intent intent = new Intent(this, PalabrasActivity.class);
